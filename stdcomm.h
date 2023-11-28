e40f6dd8478f5e8596f4b0a4dc3468c5f87c2663
z4yn1n <152300820+z4yn1n@users.noreply.github.com>
2023-11-28 14:29:59 -0500

    Create stdcomm.h

---
+++ stdcomm.h
@@ -0,0 +1,16 @@
+#ifndef stdcomm_h
+#define stdcomm_h
+
+// Function Prototypes
+int add(int, int);
+//void send_all(int, char[]);
+void send_all(int, char[], char);
+int hasNickname(char[], char[]);
+int isNicknameUnique(char[], char[]);
+void storeNickname(char[], char[], char[], char[]);
+void createWordPacket(char[], char, char[]);
+
+#endif
+
+
+
