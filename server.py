63f1f3278dbc36c94ea3ddf55ff4a6eff779d055
Anthony <124739034+ant444@users.noreply.github.com>
2023-11-28 08:56:25 -0500

    Create server.py

---
+++ server.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+###################################################################
+#
+#       Names: Anthony Nelson, Zaynin Henthorn, Adam Wisnewski
+#       Course: CSC328 (Network Programming)
+#       Semester/Year: Fall 2023
+#       Assignment #7: Final Project
+#       Short Description:
+#
+#
+#
+###################################################################
+
+
+import socket 
+import sys
+import json
+
+
+def main():
+#server code....
+
+
+
+
+if __name__ == '__main__':
+  main()
