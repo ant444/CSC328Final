ccca7049b0f4c9eb9930ce982433864130db32b1
z4yn1n <152300820+z4yn1n@users.noreply.github.com>
2023-11-28 21:45:19 -0500

    First rendition of stdcomm.c

--- stdcomm.h
+++ stdcomm.h
@@ -1,6 +1,6 @@
 #ifndef stdcomm_h
 #define stdcomm_h
-#test
+
 // Function Prototypes
 int add(int, int);
 //void send_all(int, char[]);
@@ -9,8 +9,7 @@ int hasNickname(char[], char[]);
 int isNicknameUnique(char[], char[]);
 void storeNickname(char[], char[], char[], char[]);
 void createWordPacket(char[], char, char[]);
+char* recv_word_packet(int);
+char* extract_word_packet_message(char*);
 
 #endif
-
-
-
