8f4eae9416304a9296ca3a18bb94ed8421c7a940
Awizn <104389758+Awizn@users.noreply.github.com>
2023-11-30 12:49:25 -0500

    Update server.py

--- server.py
+++ server.py
@@ -17,9 +17,47 @@ import socket
 import sys
 import json
 
+def readPackets(s, num):
+    bytes = b''
+    while len(bytes) != num:
+        read = s.recv(num - len(bytes))
+        bytes += read
+        if len(read) == 0: break
+    return bytes
 
-def main():
-#server code....
+if __name__ == "__main__":
+   if len(sys.argv) > 2:
+        exit("Too many arguments.")
+   try:
+        if len(sys.argv) == 1:
+            portNum = 3001
+        else:
+            portNum = int(sys.argv[1]))
+
+        with socket.socket() as s:
+            s.bind(portNum)
+            s.listen(1)
+            while True:
+                conn, addr = s.accept()
+                with conn:
+                    conn.sendall("HELLO")
+
+                    length = readPackets(s, 2)
+                    if length == 0:
+                        break
+                    nickname = readPackets(s, length)
+                    #zaynin's code to put nickname stuff in
+
+                while(chat != "quit"):
+                    length = readPackets(s, 2)
+                    if length == 0:
+                        break
+                    chat = readPackets(s, length)
+                    #zaynin's code for putting into log file
+    except OSError as e:
+        exit(f'{e}')
+    except KeyboardInterrupt:
+        print("DONE")
 
 
 
