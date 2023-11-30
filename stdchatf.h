72dd115b01d3123a3fcf158e314f35a504bceaf1
z4yn1n <152300820+z4yn1n@users.noreply.github.com>
2023-11-30 12:41:31 -0500

    Create stdchatf.h

---
+++ stdchatf.h
@@ -0,0 +1,11 @@
+#ifndef stdchatf_h
+#define stdchatf_h
+
+// Function Prototypes
+int hasNickname(char[], char[]);
+int isNicknameUnique(char[], char[]);
+void storeNickname(char[], char[], char[], char[]);
+//void writeToLogFile() - to be written
+//void oututLogFile() - to be written
+
+#endif
