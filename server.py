f23bcd7fdab2f0c7eb1642ce70b6a99651772056
Anthony <124739034+ant444@users.noreply.github.com>
2023-12-07 13:59:12 -0500

    Update server.py
    New server stuff

--- server.py
+++ server.py
@@ -17,12 +17,20 @@ import socket
 import sys
 from datetime import datetime
 
+
+import ctypes
+import stdwp
+stdchatf = ctypes.CDLL('./stdchatf.so')
+
+
+
 def readPackets(s, num):
     bytes = b''
     while len(bytes) != num:
         read = s.recv(num - len(bytes))
         bytes += read
         if len(read) == 0: break
+    print("READ")
     return bytes
 
 if __name__ == "__main__":
@@ -32,60 +40,70 @@ if __name__ == "__main__":
         if len(sys.argv) == 1:
             portNum = 3001
         else:
-            portNum = int(sys.argv[1]))
+            portNum = int(sys.argv[1])
 
         with socket.socket() as s:
-            s.bind('', portNum)
+            s.bind(("",portNum))
             s.listen(1)
             while True:
-                clientIP = s.gethostbyname(s.gethostbyname())
                 conn, addr = s.accept()
                 with conn:
-                    hello = create_word_packet("HELLO", 'm')
+                    clientIP = addr[0]
+                    hello = stdwp.create_word_packet("HELLO", 'm')
                     conn.sendall(hello)
 
                     #loop for asking for nickname and checking if nickname is unique
+                    isUnique = 0
                     while isUnique == 0:
-                        length = readPackets(s, 2)
+                        print("CHECK")
+                        length = readPackets(conn, 2)
                         if length == 0:
                             break
-                        skip = readPackets(s, 1)
-                        nickname = readPackets(s, length)
+                        skip = readPackets(conn, 1)
+                        nickname = readPackets(conn, length)
 
-                        isUnique = isNicknameUnique('nicknames.txt", nickname)
+                        isUnique = stdchatf.isNicknameUnique(b"nicknames.txt", nickname)
                         if isUnique == 0:
-                            retry = create_word_packet("RETRY", 'm')
+                            retry = stdwp.create_word_packet("RETRY", 'm')
                             conn.sendall(retry)
 
                     #zaynin's code to put nickname stuff in
-                    storeNickname("nicknames.txt", datetime.now().encode('utf+8'), clientIP.encode('utp+8'), nickname)
-                    ready = create_word_packet("READY", 'm')
+                    currtime = datetime.now()
+                    isoformatted = currtime.isoformat()
+                    isobytes = isoformatted.encode('utf-8')
+                    stdchatf.storeNickname(b"nicknames.txt", b"ABC", clientIP.encode('utf-8'), nickname)
+                    ready = stdwp.create_word_packet("READY", 'm')
                     conn.sendall(ready)
 
-                    while(chat != "quit" and type != c):
+                    chat = ""
+                    type = ""
+                    while(chat != "quit" and type != 'c'):
                         length = readPackets(s, 2)
                         wordPacket = length+readPackets(s, length+1)
-                        chat = extract_word_packet_message(wordPacket)
-                        type = get_word_packet_type(wordPacket)
-                        
+                        chat = stdwp.extract_word_packet_message(wordPacket)
+                        type = stdwp.get_word_packet_type(wordPacket)
+
                         if length == 0:
                             break
 
                         if type == c:
                             command_parts = chat.split()
                             if command_parts[0] == "nick":
-                                nickname = command_parts[1].encode('utf+8')
-                                isUnique = isNicknameUnique('nicknames.txt", nickname)
+                                nickname = command_parts[1].encode('utf-8')
+                                isUnique = stdchatf.isNicknameUnique(b'nicknames.txt', nickname)
                                 while isUnique == 0:
-                                    storeNickname("nicknames.txt", datetime.now().encode('utf+8'), clientIP.encode('utp+8'), nickname)
+                                    stdchat.storeNickname(b"nicknames.txt", b"ABC", clientIP.encode('utp+8'), nickname)
                                     conn.sendall(ready)
                                 else:
-                                    retry = create_word_packet("RETRY", 'm')
+                                    retry = stdwp.create_word_packet("RETRY", 'm')
                                     conn.sendall(retry)
                     #zaynin's code for putting into log file
-                        else:
+#                        else:
                          #   writeToLogFile(logfile, datetime.now().encode('utf+8'), nickname, chat)
-        except OSError as e:
+    except OSError as e:
         exit(f'{e}')
     except KeyboardInterrupt:
         print("DONE")
+
+
+
