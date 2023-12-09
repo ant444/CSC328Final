72aec24f8abd7cfb351fa558a7d7aae508eaed32
Anthony <124739034+ant444@users.noreply.github.com>
2023-12-09 13:30:06 -0500

    Update server.py
    Almost fully functional server

--- server.py
+++ server.py
@@ -61,48 +61,59 @@ if __name__ == "__main__":
                             break
                         skip = readPackets(conn, 1)
                         nickname = readPackets(conn, int.from_bytes(length, byteorder='big'))
-                        print(nickname);
+                        print(nickname)
                         isUnique = stdchatf.isNicknameUnique(b"nicknames.txt", nickname)
+                        print("GOT HERE 1")
                         if isUnique == 0:
                             retry = stdwp.create_word_packet("RETRY", 'm')
+                            print("Sent RETRY")
                             conn.sendall(retry)
+                        else:
+                            print("GOTHERE2")
+                            #zaynin's code to put nickname stuff in
+                            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            stdchatf.storeNickname(b"nicknames.txt", current_time.encode('utf-8'), clientIP.encode('utf-8'), nickname)
 
-                    #zaynin's code to put nickname stuff in
-                    currtime = datetime.now()
-                    isoformatted = currtime.isoformat()
-                    isobytes = isoformatted.encode('utf-8')
-                    stdchatf.storeNickname(b"nicknames.txt", datetime.now().encode('utf-8'), clientIP.encode('utf-8'), nickname)
-                    ready = stdwp.create_word_packet("READY", 'm')
-                    conn.sendall(ready)
-                    print("Past")
 
-                    chat = ""
-                    type = ""
-                    while(chat != "quit" and type != 'c'):
-                        length = readPackets(s, 2)
-                        wordPacket = length+readPackets(s, length+1)
+                            ready = stdwp.create_word_packet("READY", 'm')
+                            print("SENDING READY")
+                            conn.sendall(ready)
+                            print("Past")
+
+                    chat = " "
+                    type = " "
+                    while(chat != "quit" and type != b'c'):
+                        print("GOTHERE3")
+                        length = readPackets(conn, 2)
+                        print("GotHERE4")
+                        wordPacket = length + readPackets(conn, int.from_bytes(length, byteorder='big') + 1)
+                        print(wordPacket)
                         chat = stdwp.extract_word_packet_message(wordPacket)
+                        print(chat.encode())
                         type = stdwp.get_word_packet_type(wordPacket)
-
+                        print("GOTHERE5")
                         if length == 0:
                             break
 
-                        if type == c:
+                        if type == b'c':
                             command_parts = chat.split()
                             if command_parts[0] == "nick":
                                 nickname = command_parts[1].encode('utf-8')
                                 isUnique = stdchatf.isNicknameUnique(b'nicknames.txt', nickname)
                                 while isUnique == 0:
-                                    stdchat.storeNickname(b"nicknames.txt", datetime.now().encode('utf-8'), clientIP.encode('utp+8'), nickname)
+                                    stdchatf.storeNickname(b"nicknames.txt", datetime.now().encode('utf-8'), clientIP.encode('utp+8'), nickname)
                                     conn.sendall(ready)
                                 else:
                                     retry = stdwp.create_word_packet("RETRY", 'm')
                                     conn.sendall(retry)
                     #zaynin's code for putting into log file
                         else:
-                            writeToLogFile(logfile, datetime.now().encode('utf+8'), nickname, chat)
+                            print("Made it to log file stuff.")
+                            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+                            stdchatf.writeToLogFile(b"logfile.txt", current_time.encode('utf+8'), nickname, chat.encode())
     except OSError as e:
         exit(f'{e}')
     except KeyboardInterrupt:
         print("DONE")
-                    
+
+
