9f3408240eba2bf6287f678bb115f45d93420014
z4yn1n <152300820+z4yn1n@users.noreply.github.com>
2023-12-09 18:08:13 -0500

    Update stdchatf.h.
    writeToLogFile() and storeNickname() now return an integer to determine if the function succeeded or not.

--- stdchatf.h
+++ stdchatf.h
@@ -1,14 +1,18 @@
+// Author: Zaynin Henthorn
+// Major: Computer Science
+// Course: CSC 328 Fall 2023
+// Assignment: Final Group Project - Chat Server
+// Purpose: The header file for stdchatf library.
+
 #ifndef stdchatf_h
 #define stdchatf_h
 
 // Function Prototypes
 int hasNickname(char[], char[]);
 int isNicknameUnique(char[], char[]);
-void storeNickname(char[], char[], char[], char[]);
-void writeToLogFile(char[], char[], char[], char[]);
-//void oututLogFile() - to be written
+int storeNickname(char[], char[], char[], char[]);
+int writeToLogFile(char[], char[], char[], char[]);
 
 #endif
-
 
 
