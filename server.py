54d3de9a3f1355b826faeb5d7cec7bb2aa8af0ca
Awizn <104389758+Awizn@users.noreply.github.com>
2023-12-12 16:54:10 -0500

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,9 @@
 ###################################################################
 
 
+
 import functools
+import os
 import socket
 import sys
 import signal
@@ -25,10 +27,12 @@ import ctypes
 import stdwp
 stdchatf = ctypes.CDLL('./stdchatf.so')
 
-def sigint_handler(conn, signal, frame):
+def sigint_handler(conn, pid, signal, frame):
     try:
         global s
         print('Server will shut down in five seconds.')
+
+#        pid = additional_param
 
         x = 5
         sending = stdwp.create_word_packet('Server will shut down in five seconds.', 'm')
@@ -38,11 +42,16 @@ def sigint_handler(conn, signal, frame):
             x = x-1
             time.sleep(1)
 
-        conn.close()
+        if pid != 0:
+            pid, status = os.wait()
+            if os.WIFEXITED(status) == False:
+                print("Error given when exiting")
+            conn.close()
+
+#        conn.close()
         sys.exit(0)
     except Exception as e:
         print(f'Error in sigint_handler: {e}')
-
 
 def readPackets(s, num):
     bytes = b''
@@ -52,7 +61,8 @@ def readPackets(s, num):
         if len(read) == 0: break
     print("READ")
     return bytes
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     if len(sys.argv) > 2:
         exit("Too many arguments.")
     try:
@@ -62,16 +72,24 @@ def readPackets(s, num):
             portNum = int(sys.argv[1])
 
         with socket.socket() as s:
-#            signal.signal(signal.SIGINT, sigint_handler)
             s.bind(("",portNum))
             s.listen(5)
+
+            for x in range(1,6):
+                try:
+                    pid = os.fork()
+                except OSError as err:
+                    print("Error with creating a new process: ", err.strerror)
+                    sts.exit(-1)
+
             while True:
+#                if pid == 0:
                 conn, addr = s.accept()
                 with conn:
-#                    signal.signal(signal.SIGINT, sigint_handler)
-                    #custom_param_value = 5  # Set your desired parameter value here
-                    sigint_handler_with_param = functools.partial(sigint_handler, conn)
+
+                    sigint_handler_with_param = functools.partial(sigint_handler, conn, pid)
                     signal.signal(signal.SIGINT, sigint_handler_with_param)
+                    
                     clientIP = addr[0]
                     hello = stdwp.create_word_packet("HELLO", 'm')
                     conn.sendall(hello)
@@ -79,7 +97,7 @@ def readPackets(s, num):
                     #loop for asking for nickname and checking if nickname is unique
                     isUnique = 0
                     while isUnique == 0:
-                        print("CHECK")
+#                        print("CHECK")
                         length = readPackets(conn, 2)
                         if length == 0:
                             break
@@ -87,13 +105,13 @@ def readPackets(s, num):
                         nickname = readPackets(conn, int.from_bytes(length, byteorder='big'))
                         print(nickname)
                         isUnique = stdchatf.isNicknameUnique(b"nicknames.txt", nickname)
-                        print("GOT HERE 1")
+#                        print("GOT HERE 1")
                         if isUnique == 0:
                             retry = stdwp.create_word_packet("RETRY", 'm')
                             print("Sent RETRY")
                             conn.sendall(retry)
                         else:
-                            print("GOTHERE2")
+#                            print("GOTHERE2")
                             #zaynin's code to put nickname stuff in
                             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                             stdchatf.storeNickname(b"nicknames.txt", current_time.encode('utf-8'), clientIP.encode('utf-8'), nickname)
@@ -102,13 +120,14 @@ def readPackets(s, num):
                             ready = stdwp.create_word_packet("READY", 'm')
                             print("SENDING READY")
                             conn.sendall(ready)
-                            print("Past")
+#                            print("Past")
 
                     chat = " "
                     type = " "
                     while(chat != 'BYE' and type != 'c'):
 #                        print("GOTHERE3")
                         length = readPackets(conn, 2)
+#                        print("GotHERE4")
                         wordPacket = length + readPackets(conn, int.from_bytes(length, byteorder='big') + 1)
                         print(wordPacket)
                         chat = stdwp.extract_word_packet_message(wordPacket)
@@ -128,7 +147,7 @@ def readPackets(s, num):
                                 while isUnique == 0:
                                     stdchatf.storeNickname(b"nicknames.txt", datetime.now().encode('utf-8'), clientIP.encode('utp+8'), nickname)
                                     conn.sendall(ready)
-                                else:
+                                    else:
                                     retry = stdwp.create_word_packet("RETRY", 'm')
                                     conn.sendall(retry)
                     #zaynin's code for putting into log file
@@ -146,7 +165,7 @@ def readPackets(s, num):
 
                                 # Get the last line
                                 last_line = lines[-1]
-                                # Send most recent chat message to all clients
+                            # Send most recent chat message to all clients
                             formatted_sendback_wp = stdwp.format_logfile_entry(last_line)
                             sendback_wp = stdwp.create_word_packet(formatted_sendback_wp, "l")
                             conn.sendall(sendback_wp)
@@ -155,4 +174,4 @@ def readPackets(s, num):
     except OSError as e:
         exit(f'{e}')
     except KeyboardInterrupt:
-        print("DONE")
+        x = 0
