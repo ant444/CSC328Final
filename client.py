f96d00aff81c1b2cc76dac1e14617554ac87940d
Anthony <124739034+ant444@users.noreply.github.com>
2023-12-12 00:09:13 -0500

    Update client.py

--- client.py
+++ client.py
@@ -19,26 +19,32 @@
 #       You have the choice to make this push-based or pull-based.
 #
 ####################################################################
-
 import socket
 import sys
 import ctypes
 import os
 import stdwp
-from multiprocessing import Process, Pipe
+
 stdchatf = ctypes.CDLL('./stdchatf.so')
 
-
+def child_process(s, pout):
+    global child_process_finished
+    while True:
+        word_data = receive_word_packet(s)
+        print(word_data)
+        if word_data == "BYE" or word_data == "1":
+            child_process_finished = True
+            s.close()
+            pout.write("f")
+            pout.flush()
+            exit()
 
 def receive_word_packet(s):
-    word_length = int.from_bytes(s.recv(2), byteorder='big')  # Receive the word length
+    word_length = int.from_bytes(s.recv(2), byteorder='big')
     skip = s.recv(1)
-    word_packet = s.recv(word_length)  # Receive the word packet based on the length received
+    word_packet = s.recv(word_length)
     decodedwordpacket = word_packet.decode('utf-8')
-    #print(decodedwordpacket)
     return decodedwordpacket
-
-
 
 def recv_chat_msg(s):
     while True:
@@ -46,20 +52,18 @@ def recv_chat_msg(s):
         if not chat_packet:
             break
         chat_message = chat_packet.decode('utf-8')
-#        if chat_message == "1":
-#            s.close()
-#            exit()
+        if chat_message == "1":
+            s.close()
+            os._exit(0)
         print(f'Received: {chat_message}')
 
-
-
 def send_nickname(s):
-	while True:
+    while True:
         nickname = input("Please enter a nickname: ")
         if 2 < len(nickname) < 17:
             nick_length = len(nickname)
             typemessage = b'c'
-            bytenick = nick_length.to_bytes(2, byteorder = 'big')
+            bytenick = nick_length.to_bytes(2, byteorder='big')
             nickname_bytes = bytes(nickname, 'utf-8')
             nicknamewordpacket = bytenick + typemessage + nickname_bytes
             s.sendall(nicknamewordpacket)
@@ -67,23 +71,29 @@ def send_nickname(s):
         else:
             print("Nickname should be 3-16 characters long. Retry:")
 
-def ready_or_retry(s,nickname):
+def ready_or_retry(s, nickname):
     while True:
         ready_or_not = receive_word_packet(s)
         if ready_or_not == "READY":
             print(f'Welcome, {nickname} ')
             chat = input(f'Please enter a chat message, {nickname}: ')
-	    chatlength = len(chat)
-            typechat = b't'
-            bytechat = chatlength.to_bytes(2, byteorder = 'big')
-            chatbytes = bytes(chat, 'utf-8')
-            chatwordpacket = bytechat + typechat + chatbytes
-            s.sendall(chatwordpacket)
-            break
+            if chat == '/quit':
+                print("Sending BYE to the server.")
+                bye = stdwp.create_word_packet("BYE", 'm')
+                s.sendall(bye)
+                s.close()
+                os._exit(0)
+            else:
+                chatlength = len(chat)
+                typechat = b't'
+                bytechat = chatlength.to_bytes(2, byteorder='big')
+                chatbytes = bytes(chat, 'utf-8')
+                chatwordpacket = bytechat + typechat + chatbytes
+                s.sendall(chatwordpacket)
+                break
         elif ready_or_not == "RETRY":
             while True:
                 newnick = input("This nickname is in use. Please Retry: ")
-                #Send it over to the client, type is t
                 if 2 < len(newnick) < 17 and newnick != nickname:
                     print("Welcome, ", newnick)
                     send_nickname(s, newnick)
@@ -91,82 +101,86 @@ def ready_or_retry(s,nickname):
                     break
                 else:
                     print("Nickname can only be 3-17 characters long. Retry:")
-                    newnicklen = len(newnick)
-		    type = b'c'
-                    bytenewnick = newnicklen.to_bytes(2, byteorder = 'big')
-                    newnickbytes = bytes(newnick, 'utf-8')
-                    newnickwordpacket = bytenewnick + type + newnickbytes
-                    s.sendall(newnickwordpacket)
 
 def main():
+    global child_process_finished
+
     if len(sys.argv) < 2:
         print('Usage: python script.py <host> <port>')
         return
 
     host = sys.argv[1]
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 18008
 
-
-    if len(sys.argv) > 2:
-        try:
-            port = int(sys.argv[2])
-        except ValueError:
-            print("Port has to be an integer!!!!!")
-	    return
-    else:
-        port = 18008 #Defaulting the port
     try:
         with socket.socket() as s:
             s.connect((host, port))
-#            pid = os.fork()
             nickname_sent = False
             word_data = receive_word_packet(s)
             if not nickname_sent:
                 nickname = send_nickname(s)
                 nickname_sent = True
 
-            ready_or_retry(s,nickname)
+            ready_or_retry(s, nickname)
 
-            # Zaynin's Code: forking
-            child_pid = os.fork()
-            #parent_conn, child_conn = Pipe()          # create pipes to communicate between parent and child
-	    if child_pid == 0:
-                # This code is in the child process
+            #child_process_finished.value = 0
+            #child_proc = multiprocessing.Process(target=child_process, args=(s, child_conn))
+            #child_proc.start()
 
-                # RECEIVE WORD PACKETS FROM SERVER - updates chats from all clients in real time
-                while True:
-                    word_data = receive_word_packet(s)
-                    if word_data == "1":
-                        print(word_data)
-                        s.close()
-                        exit()
-                    if(word_data == 'BYE'):
-                        s.close()
-                        exit()
-                    else:
-                        print(word_data)
+            ########
+            #
+            #Used Dr. Schwesinger's pipe code as a reference for this...
+            #
+            ############
 
+
+            pr, cw = os.pipe()
+            cr, pw = os.pipe()
+
+            pid = os.fork()
+
+            if pid == 0:
+                os.close(pr)
+                os.close(pw)
+                child_process(s, os.fdopen(cw, 'w'))
             else:
-                # This code is in the parent process
-		# RECEIVE CHAT MESSAGES FROM USER
-                while True:
-                    #print("To quit the chat server, type '/quit'")
-                    newchatmsg = input() #(f"{nickname}'s chat message: ") - weird output when this is uncommented
-                    if newchatmsg.lower() == '/quit':
-                        print("Sending BYE to the server...")
-                        bye = stdwp.create_word_packet("BYE", 'c')
-                        s.sendall(bye)
-                        s.close()
-                        exit()
-                    else:
-                        chatwrdpacket = stdwp.create_word_packet(newchatmsg, 't')
-                        s.sendall(chatwrdpacket)
+                os.close(cr)
+                os.close(cw)
+                pin = os.fdopen(pr, 'r')
+                pout = os.fdopen(pw, 'w')
 
+                data = pin.readline()
+                if data == "f":
+                    print("Goodbye.")
+                    exit()
+                else:
+                    print("Error in exiting.")
+                    exit()
+
+            while True:
+                if child_process_finished.value:
+                    print("Child process finished.")
+                    s.close()
+                    os._exit(0)
+
+                newchatmsg = input(f"{nickname}'s chat message: ")
+                if child_process_finished.value == 1 or newchatmsg.lower() == '/quit':
+                    print("Sending BYE to the server...")
+                    bye = stdwp.create_word_packet("BYE", 'c')
+                    s.sendall(bye)
+                    s.close()
+                    os._exit(0)
+                else:
+                    chatwrdpacket = stdwp.create_word_packet(newchatmsg, 't')
+                    s.sendall(chatwrdpacket)
 
     except KeyboardInterrupt:
-        print("Keyboard Interrupt:Program Terminated")
+        print("Keyboard Interrupt: Program Terminated")
     except Exception as e:
         print(f'An error occurred: {e}')
 
 if __name__ == '__main__':
     main()
+
+
 
